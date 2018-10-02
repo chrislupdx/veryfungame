e07fd5e9123be9d878faf17749ac3807bb8fc598
chrislupdx <christopherlu.pdx@gmail.com>
2018-10-01 17:31:27 -0700

    added internal scoring, need other things next

--- game.js
+++ game.js
@@ -2,7 +2,18 @@ function poke(id){
 	console.log(id)
 	var element = document.getElementById(id);
 	 element.classList.toggle("woke");
+	incrementcount();
+
 }
 
-let score : [0];
+let player_score = [];
 
+function addNum(num) {
+      numArray.push(num);
+      document.querySelector(".txt").innerHTML = numArray.join('');
+  }
+
+function incrementcount(){
+	player_score++;
+	console.log(player_score)
+}
