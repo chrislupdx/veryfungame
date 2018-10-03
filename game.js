e7a1d07afcc0409e8fbe4a9581276827954a7fff
chrislupdx <christopherlu.pdx@gmail.com>
2018-10-03 15:53:04 -0700

    fixed up point values, there are actual m/h(ole)s, next: build out game stop

--- game.js
+++ game.js
@@ -1,34 +1,48 @@
 hole_id_list = ['1', '2', '3', '4', '5', '6','7','8', '9'
 ];
 
-let time = new Date();
-let player_score = [];
 let points = 0;
 let gameend = false;
 
+let time = new Date();
+time.setHours(0, 0, 0, 0);
+
+playtime = setInterval(flip_critters, 1000);
+
+
 function poke(id){
 	var element = document.getElementById(id);
-	element.classList.toggle("woke");
+	element.classList.toggle("box");
 }
 
 function flip_critters(){
 	random_pick = hole_id_list[Math.floor(Math.random() * hole_id_list.length)];
-	$(`#${random_pick}`).attr("src", "mole.png");
-
-	console.log(random_pick);
+	$(`#${random_pick}`).toggleClass("woke");
+	
+	let index = hole_id_list.indexOf(random_pick);
+	hole_id_list.splice(index, 1);
+	
+	if(hole_id_list.length=== 0){
+		$('#end').show();
+		window.clearInterval(playtime);
+		clearInterval(playtime);
+		gameend = true;
+	}
 }
 
-
+//this handles points/displaying score
 $(".box").on('click', function(){
 	if (!gameend){
 		if ($(this).hasClass('box')){
 			points -= 50;
-			console.log(points)
+			$('#points').text(`Score: ${points}`);
 		}
 		if ($(this).hasClass('woke')){
 			points += 100;
-			console.log(points)
+			$('#points').text(`Score: ${points}`);
 		}
 	}
+
 });
 
+$('#gameend').hide();
