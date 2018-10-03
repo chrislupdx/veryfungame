00fdcacf01ea262f0b411f6d589c0e4abdaa307a
chrislupdx <christopherlu.pdx@gmail.com>
2018-10-02 17:45:56 -0700

    got scoring to work and started to jquery stuff, next is timing+getting moles to show/endgame

--- game.js
+++ game.js
@@ -1,19 +1,34 @@
-function poke(id){
-	console.log(id)
-	var element = document.getElementById(id);
-	 element.classList.toggle("woke");
-	incrementcount();
+hole_id_list = ['1', '2', '3', '4', '5', '6','7','8', '9'
+];
 
-}
-
+let time = new Date();
 let player_score = [];
+let points = 0;
+let gameend = false;
 
-function addNum(num) {
-      numArray.push(num);
-      document.querySelector(".txt").innerHTML = numArray.join('');
-  }
-
-function incrementcount(){
-	player_score++;
-	console.log(player_score)
+function poke(id){
+	var element = document.getElementById(id);
+	element.classList.toggle("woke");
 }
+
+function flip_critters(){
+	random_pick = hole_id_list[Math.floor(Math.random() * hole_id_list.length)];
+	$(`#${random_pick}`).attr("src", "mole.png");
+
+	console.log(random_pick);
+}
+
+
+$(".box").on('click', function(){
+	if (!gameend){
+		if ($(this).hasClass('box')){
+			points -= 50;
+			console.log(points)
+		}
+		if ($(this).hasClass('woke')){
+			points += 100;
+			console.log(points)
+		}
+	}
+});
+
