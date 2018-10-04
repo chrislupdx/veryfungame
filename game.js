b74aa7983430aedc035e3331d19cf458a0beef37
chrislupdx <christopherlu.pdx@gmail.com>
2018-10-03 18:05:28 -0700

    got clicks to work, need to get my logic for classes unified then I'm done

--- game.js
+++ game.js
@@ -4,45 +4,41 @@ hole_id_list = ['1', '2', '3', '4', '5', '6','7','8', '9'
 let points = 0;
 let gameend = false;
 
-let time = new Date();
-time.setHours(0, 0, 0, 0);
-
 playtime = setInterval(flip_critters, 1000);
-
 
 function poke(id){
 	var element = document.getElementById(id);
-	element.classList.toggle("box");
+	$("#" + element.id + "").toggleClass('hole mole');
+	hole_id_list.push(element.id);
+	console.log(element)
 }
 
 function flip_critters(){
 	random_pick = hole_id_list[Math.floor(Math.random() * hole_id_list.length)];
-	$(`#${random_pick}`).toggleClass("woke");
+	$(`#${random_pick}`).toggleClass('mole hole');
 	
 	let index = hole_id_list.indexOf(random_pick);
 	hole_id_list.splice(index, 1);
 	
 	if(hole_id_list.length=== 0){
-		$('#end').show();
 		window.clearInterval(playtime);
 		clearInterval(playtime);
 		gameend = true;
+		$('#gameend').show();
 	}
 }
 
 //this handles points/displaying score
-$(".box").on('click', function(){
+$('img').on('click', function(){
 	if (!gameend){
-		if ($(this).hasClass('box')){
+		if ($(this).hasClass('hole')){
 			points -= 50;
-			$('#points').text(`Score: ${points}`);
 		}
-		if ($(this).hasClass('woke')){
+		if ($(this).hasClass('mole')){
 			points += 100;
-			$('#points').text(`Score: ${points}`);
 		}
+	$('#points').text(`${points}`);
 	}
-
 });
 
 $('#gameend').hide();
